5e5dfc69adef78f1dc9f433d85638e413598bbb8
Scott Steinhauser <scottsteinhauser@connect-172-31-20-236.vpn.tamu.edu>
2021-11-17 09:52:47 -0600

    Added ant robot

--- habitat_sim/robots/__init__.py
+++ habitat_sim/robots/__init__.py
@@ -4,11 +4,16 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from habitat_sim.robots.fetch_robot import FetchRobot, FetchRobotNoWheels
+from habitat_sim.robots.fetch_robot import FetchRobot
 from habitat_sim.robots.mobile_manipulator import (
     MobileManipulator,
     MobileManipulatorParams,
     RobotCameraParams,
+)
+from habitat_sim.robots.ant_robot import AntV2Robot
+from habitat_sim.robots.quadruped_wrapper import (
+    QuadrupedRobotParams,
+    QuadrupedRobot
 )
 from habitat_sim.robots.robot_interface import RobotInterface
 
@@ -17,6 +22,8 @@ __all__ = [
     "MobileManipulatorParams",
     "MobileManipulator",
     "FetchRobot",
-    "FetchRobotNoWheels",
     "RobotCameraParams",
+    "QuadrupedRobotParams",
+    "QuadrupedRobot",
+    "AntV2Robot",
 ]
